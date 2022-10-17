0d3654f1a4a425fc15aac500ab6b38b835ae892f
Noela-Murugi <murugin254@gmail.com>
2022-10-17 23:59:07 +0300

    completed theassignment

---
+++ app.rb
@@ -0,0 +1,20 @@
+# app.rb
+puts "Hello world!"
+puts "Hello world!"
+puts "Hello world!"
+
+print "Hello world!"
+print "Hello world!"
+print "Hello world!"
+
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+
+# puts "Hello IRB"
+
+# pass tests
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
+
